6d815da8482c1cf4f4fbc216e1d35393f0345204
Roberto10Andrade <n11.roberto11@gmail.com>
2025-12-25 19:42:40 -0300

    Fix: separate client components from static params

--- app/atividades/[id]/page.tsx
+++ app/atividades/[id]/page.tsx
@@ -1,6 +1,3 @@
-'use client'
-
-import { use } from 'react'
 import ActivityDetails from '@/app/components/ActivityDetails'
 
 export function generateStaticParams() {
@@ -20,8 +17,8 @@ interface ActivityPageProps {
   params: Promise<{ id: string }>
 }
 
-export default function ActivityPage({ params }: ActivityPageProps) {
-  const { id } = use(params)
+export default async function ActivityPage({ params }: ActivityPageProps) {
+  const { id } = await params
 
   return (
     <div className="container mx-auto px-4 py-8">
